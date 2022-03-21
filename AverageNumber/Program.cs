25a24aacb7d9f87b4d05a493db3caa8452b6f6be
Jovanoskaelena <101874636+Jovanoskaelena@users.noreply.github.com>
2022-03-21 14:23:44 +0100

    Add project files.

---
+++ AverageNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double NumberOne, NumberTwo, NumberThree, NumberFour;
+            Console.WriteLine("Enter the First Number:");
+            NumberOne = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number:");
+            NumberTwo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the third Number:");
+            NumberThree = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the fourth Number:");
+            NumberFour = Convert.ToDouble(Console.ReadLine());
+
+            double average = (NumberOne + NumberTwo + NumberThree + NumberFour) / 4;
+            Console.WriteLine("The average of {0}, {1}, {2}, {3} is: {4} ",
+            NumberOne, NumberTwo, NumberThree, NumberFour, average);
+            Console.ReadLine();
+        }
+    }
+}
